8a5b9894edcc502ec1fa1415447b214ace5cb56f
yintengzhao <2500903628@qq.com>
2018-05-20 20:58:04 +0800

    用户模块基本框架搭建完成

--- app/src/ts/require_sim_ctrl.ts
+++ app/src/ts/require_sim_ctrl.ts
@@ -3,5 +3,72 @@ import { ng_app } from './ng_app';
 
 ng_app.controller("RequireSimCtrl", ['$scope', '$interval', '$timeout', '$window', '$http', '$sce',
   function($scope, $interval, $timeout, $window, $http, $sce) {
+    $scope.rec=function()
+    {
+      $scope.first_flag=false;
+      $scope.second_flag=false;
+      $scope.third_flag=false;
+      $scope.fourth_flag=false;
+      $scope.fifth_flag=false;
+      $scope.sixth_flag=false;
+
+
+    }
+    $scope.first=function()
+    {
+      $scope.first_flag=true;
+      $scope.second_flag=false;
+      $scope.third_flag=false;
+      $scope.fourth_flag=false;
+      $scope.fifth_flag=false;
+      $scope.sixth_flag=false;
+
+    }
+    $scope.second=function()
+    {
+      $scope.first_flag=false;
+      $scope.second_flag=true;
+      $scope.third_flag=false;
+      $scope.fourth_flag=false;
+      $scope.fifth_flag=false;
+      $scope.sixth_flag=false;
+    }
+    $scope.third=function()
+    {
+      $scope.first_flag=false;
+      $scope.second_flag=false;
+      $scope.third_flag=true;
+      $scope.fourth_flag=false;
+      $scope.fifth_flag=false;
+      $scope.sixth_flag=false;
+    }
+    $scope.fourth=function()
+    {
+      $scope.first_flag=false;
+      $scope.second_flag=false;
+      $scope.third_flag=false;
+      $scope.fourth_flag=true;
+      $scope.fifth_flag=false;
+      $scope.sixth_flag=false;
+    }
+    $scope.fifth=function()
+    {
+      $scope.first_flag=false;
+      $scope.second_flag=false;
+      $scope.third_flag=false;
+      $scope.fourth_flag=false;
+      $scope.fifth_flag=true;
+      $scope.sixth_flag=false;
+    }
+    $scope.sixth=function()
+    {
+      $scope.first_flag=false;
+      $scope.second_flag=false;
+      $scope.third_flag=false;
+      $scope.fourth_flag=false;
+      $scope.fifth_flag=false;
+      $scope.sixth_flag=true;
+    }
+    $window.onclick=function(){}
 
 }
