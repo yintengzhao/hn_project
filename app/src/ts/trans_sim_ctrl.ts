14a3444d94ba6601e41b15f9a9a70a257736be1c
yintengzhao <2500903628@qq.com>
2018-05-22 19:59:29 +0800

    angular图片上传完成

--- app/src/ts/trans_sim_ctrl.ts
+++ app/src/ts/trans_sim_ctrl.ts
@@ -52,95 +52,37 @@ ng_app.controller("TransSimCtrl", ['$scope', '$interval', '$timeout', '$window',
       }
     };
     // 增删改查部分.............................................................
+$scope.save=function(){
+  var fd = new FormData();
+        var file = document.querySelector('input[type=file]').files[0];
+        fd.append('file', file);
+  var workid={
+            method:'POST',
+            url:'http://10.134.45.94:8080/ProductCenter/uploadImage',
+            // params: { file: $scope.myFile },
+            data: fd,
+            headers: {'Content-Type':undefined},
+            transformRequest: angular.identity,
+          }
+          $http(workid).then(function(response){},function(){alert('err')});
+}
+// 添加模块
+$scope.add=function(){
+  var workid={
+            method:'POST',
+            url:'http://10.134.45.94:8080/ProductCenter/addProduct',
+            params: {type:$scope.type;apply:$scope.apply;parameter:$scope.parameter;firstclass:$scope.firstclass;secondclass:$scope.secondclass;image:$scope.image},
+          }
+          $http(workid).then(function(response){console.log(response)},function(){alert('err')});
+}
 
 
-    // $scope.uploadFile = function(event) {
-    //             var files = event.target.files[0];
-    //             document.querySelector("#upload").value = "";
-    //             var fileSize = files.size;
-    //             var size = fileSize / 1024;
-    //             if(size > 10000) {
-    //                 tipsMsg("上传的图片不能大于10M");
-    //                 return
-    //             }
-    //             var reqURL = "";
-    //             var params = {};
-    //             var data = new FormData();
-    //             data.append('image', files);
-    //             $http({
-    //                 method: 'post',
-    //                 url: reqURL,
-    //                 params: params,
-    //                 data: data,
-    //                 headers: {
-    //                     'Content-Type': undefined
-    //                 },
-    //                 transformRequest: angular.identity
-    //             }).then(function successCallback(response) {
-    //                 console.log(response);
-    //             }, function errorCallback(response) {
-    //                 console.log(response);
-    //             });
-    //         }
-//     $scope.reader = new FileReader();   //创建一个FileReader接口
-//     $scope.form = {     //用于绑定提交内容，图片或其他数据
-//         image:{},
-//     };
-//     $scope.thumb = {};      //用于存放图片的base64
-//     $scope.thumb_default = {    //用于循环默认的‘加号’添加图片的框
-//         1111:{}
-//     };
-//
-//     $scope.img_upload = function(files) {       //单次提交图片的函数
-//         $scope.guid = (new Date()).valueOf();   //通过时间戳创建一个随机数，作为键名使用
-//         $scope.reader.readAsDataURL(files[0]);  //FileReader的方法，把图片转成base64
-//         $scope.reader.onload = function(ev) {
-//             $scope.$apply(function(){
-//                 $scope.thumb[$scope.guid] = {
-//                     imgSrc : ev.target.result,  //接收base64
-//                 }
-//             });
-//         };
-//
-//         var data = new FormData();      //以下为像后台提交图片数据
-//         data.append('image', files[0]);
-//         data.append('guid',$scope.guid);
-//         $http({
-//             method: 'post',
-//             url: 'http://10.134.45.94:8080/ProductCenter/uploadImage',
-//             data:data,
-//             headers: {'Content-Type': undefined},
-//             transformRequest: angular.identity
-//         }).success(function(data) {
-//             if (data.result_code == 'SUCCESS') {
-//                 $scope.form.image[data.guid] = data.result_value;
-//                 $scope.thumb[data.guid].status = 'SUCCESS';
-//                 console.log($scope.form)
-//             }
-//             if(data.result_code == 'FAIL'){
-//                 console.log(data)
-//             }
-//         })
-//     };
-//
-//     $scope.img_del = function(key) {    //删除，删除的时候thumb和form里面的图片数据都要删除，避免提交不必要的
-//         var guidArr = [];
-//         for(var p in $scope.thumb){
-//             guidArr.push(p);
-//         }
-//         delete $scope.thumb[guidArr[key]];
-//         delete $scope.form.image[guidArr[key]];
-//     };
-//     $scope.submit_form = function(){    //图片选择完毕后的提交，这个提交并没有提交前面的图片数据，只是提交用户操作完毕后，
-// // 　　　　　　　　　　　　　　　　　　　　　　　　到底要上传哪些，通过提交键名或者链接，后台来判断最终用户的选择,整个思路也是如此
-//         $http({
-//             method: 'post',
-//             url: 'http://10.134.45.94:8080/ProductCenter/uploadImage',
-//             data:$scope.form,
-//         }).success(function(data) {
-//             console.log(data);
-//         })
-//     };
+
+
+
+
+
+
 
 
 
