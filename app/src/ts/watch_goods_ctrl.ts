bca57bd3b0520874137aafd92dd9f8867449545a
yintengzhao <2500903628@qq.com>
2018-05-24 18:24:25 +0800

    暂时修复格式bug

--- app/src/ts/watch_goods_ctrl.ts
+++ app/src/ts/watch_goods_ctrl.ts
@@ -1,7 +1,18 @@
 import { ng_app } from './ng_app';
 // angular.element('[ng-controller=MainCtrl]').scope()
 
-ng_app.controller("WatchGoodsCtrl", ['$scope', '$interval', '$timeout', '$window', '$http', '$sce',
-  function($scope, $interval, $timeout, $window, $http, $sce) {
+ng_app.controller("WatchGoodsCtrl", ['$scope', '$interval', '$timeout', '$window', '$http', '$sce','$location',
+  function($scope, $interval, $timeout, $window, $http, $sce,$location) {
+console.log($location.absUrl());
+var receiveId=$location.absUrl().split("=").pop();
+
+var workid={
+          method:'POST',
+          url:'http://10.134.45.94:8080/ProductCenter/getProduct',
+          params: {id:receiveId},
+        }
+        $http(workid).then(function(response){
+          $scope.allpros = response.data;
+        },function(){});
 
 }
